f71ae6397440c6232e55a3f3d5425914cffaa3a0
Nicolas <nislederz@gmail.com>
2019-01-31 10:02:01 -0500

    Test

--- Sales/Sales.Android/MainActivity.cs
+++ Sales/Sales.Android/MainActivity.cs
@@ -12,6 +12,7 @@ namespace Sales.Droid
     [Activity(Label = "Sales", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+       
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
